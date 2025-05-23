eb14ba06d340eef77a225e018ed41defe1848a88
ACHUNP1 <ACHUNP@correo.url.edu.gt>
2025-05-22 20:09:10 -0600

    Add files via upload

---
+++ SEMANA10/Semana10_2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class Semana10_2
+{
+   
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[8];
+        Console.WriteLine("por favor ingrese8 numeros");
+
+        for (int i = 0; i < 8; i++)
+        {
+            bool Valido = false;
+            while (!Valido)
+            {
+                Console.Write($"numero {i + 1}: ");
+                string entrada = Console.ReadLine();
+
+                // Verifica si es un número y si es positivo
+                if (int.TryParse(entrada, out int numero) && numero >= 0)
+                {
+                    numeros[i] = numero;
+                    Valido = true;
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, ingrese un numero positivo");
+                }
+            }
+        }
+
+        
+        Console.WriteLine("\nNúmeros ingresados:");
+        for (int i = 0; i < 8; i++)
+        {
+            Console.Write(numeros[i] + " ");
+        }
+
+        
+        int suma = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            suma += numeros[i];
+        }
+        double promedio = suma / 8.0;
+
+        Console.WriteLine($"\nsuma total: {suma}");
+        Console.WriteLine($"promedio: {promedio:F2}");
+
+        Console.ReadLine(); 
+    }
+}
+}
